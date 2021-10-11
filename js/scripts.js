2c66b8090c0ca4263f890eaf4517b988b56593be
simolex <simolex@yandex.ru>
2021-10-11 17:08:32 +0300

    four lesson

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
 const schemeSvg = document.querySelector(".scheme-svg");
 const totalPriceTag = document.querySelector(".price-total");
-let cost=800;
-let totalPrice=0;
-schemeSvg.addEventListener('click',(event)=>{
-   if(!event.target.classList.contains('booked')){
-       event.target.classList.toggle("active");
-       let totalSeats = schemeSvg.querySelectorAll(".active").length
-       totalPrice = cost * totalSeats;
-       totalPriceTag.textContent =  totalPrice;
-   }
-  
-})
+const menuButton = document.querySelector(".m-menu");
+const menu = document.querySelector(".menu");
+let cost = 800;
+let totalPrice = 0;
+schemeSvg.addEventListener("click", (event) => {
+  if (!event.target.classList.contains("booked")) {
+    event.target.classList.toggle("active");
+    let totalSeats = schemeSvg.querySelectorAll(".active").length;
+    totalPrice = cost * totalSeats;
+    totalPriceTag.textContent = totalPrice;
+  }
+});
+
+menuButton.addEventListener("click", (event) => {
+  menu.classList.toggle("is-open");
+});
